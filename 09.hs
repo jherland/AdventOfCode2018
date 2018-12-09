477d6ceedeaf33665d237f5e08d0df36c4f18745
Johan Herland <johan@herland.net>
2018-12-09 13:59:07 +0100

    Day 9 solved in Haskell

---
+++ 09.hs
@@ -0,0 +1,68 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-12.10 script
+
+{-# OPTIONS_GHC -Wall #-}
+
+import Data.Char (isDigit)
+import Data.IntMap (IntMap)
+import qualified Data.IntMap.Strict as IntMap
+import Data.Tuple (swap)
+import Text.ParserCombinators.ReadP
+
+digits :: ReadP Int
+digits = read <$> many1 (satisfy isDigit)
+
+parse :: String -> (Int, Int) -- "{} players; last marble is worth {} points"
+parse = fst . head . readP_to_S go where
+  go = do
+    p <- digits <* string " players; last marble is worth "
+    m <- digits <* string " points\n" <* eof
+    return (p, m)
+
+type Marblez = ([Int], [Int]) -- (rwd, fwd)
+type Scores = IntMap Int -- player -> score
+type Game = (Marblez, Scores) -- (marbles, players)
+
+initGame :: Int -> Game
+initGame n = (([], [0]), IntMap.fromDistinctAscList [(i, 0) | i <- [1 .. n]])
+
+rot :: Int -> Marblez -> Marblez
+rot n (rwd, fwd)
+    | n == 0    = (rwd, fwd)
+    | n < 0     = swap $ rot (abs n) (fwd, rwd)
+    | null fwd  = rot n ([], reverse rwd)
+    | otherwise = rot (n - 1) (head fwd : rwd, drop 1 fwd)
+
+place :: Int -> Marblez -> Marblez
+place m (rwd, fwd) = (rwd', m:fwd') where
+    (rwd', fwd') = rot 2 (rwd, fwd)
+
+remove :: Marblez -> (Marblez, Int)
+remove (rwd, fwd) = ((rwd', drop 1 fwd'), head fwd') where
+    (rwd', fwd') = rot (-7) (rwd, fwd)
+
+turn :: Int -> Int -> Game -> Game
+turn player marble (mz, scores) = if marble `mod` 23 /= 0
+    then (place marble mz, scores)
+    else (mz', scores') where
+        (mz', score) = remove mz
+        scores' = IntMap.adjust (+(marble + score)) player scores
+
+play :: Int -> Int -> Game
+play numPlayers numMarbles =
+    foldl (\g (p, m) -> turn p m g) game (zip players marbles) where
+        players = cycle [1 .. numPlayers]
+        marbles = [1 .. numMarbles]
+        game = initGame numPlayers
+
+highScore :: Game -> Int
+highScore (_, scores) = IntMap.foldr max 0 scores
+
+main :: IO ()
+main = do
+    input <- readFile "09.input"
+    let (nplayers, hmarble) = parse input
+    -- part 1
+    print $ highScore $ play nplayers hmarble
+    -- part 2
+    print $ highScore $ play nplayers (hmarble * 100)
